3a2ef7a2d100a62c4502ddafcc29dd8c845f7d20
Shubham Patrick <ahthneeuhl@gmail.com>
2022-05-13 23:11:58 +0530

    Merge Two Sorted Lists

---
+++ LinkedList/mergeTwoLists.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+function mergeTwoLists(list1, list2) {
+  if (list1 == null) return list2;
+  if (list2 == null) return list1;
+  if (list1.val < list2.val) {
+    list1.next = mergeTwoLists(list1.next, list2);
+    return list1;
+  } else {
+    list2.next = mergeTwoLists(list1, list2.next);
+    return list2;
+  }
+}
