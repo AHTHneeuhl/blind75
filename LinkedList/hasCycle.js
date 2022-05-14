e04146758856c8cb211fcee8945ecc1dc04ab7e6
Shubham Patrick <ahthneeuhl@gmail.com>
2022-05-14 18:37:08 +0530

    Linked List Cycle

---
+++ LinkedList/hasCycle.js
@@ -0,0 +1,12 @@
+function hasCycle(head) {
+  var slow = head;
+  var fast = head;
+
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) return true;
+  }
+
+  return false;
+}
